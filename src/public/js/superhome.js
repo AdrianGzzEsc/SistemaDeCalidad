77b31ca431b4d5afcbf7a98c27b885404a3d401b
MarceloFlores95 <luismarcelofc@gmail.com>
2020-10-26 15:47:17 -0600

    Se agrego la forma para que el super usuario pueda hacer log in

---
+++ src/public/js/superhome.js
@@ -0,0 +1,46 @@
+function validate() {
+    let url = "/user/validate-user";
+    let settings = {
+        method : 'GET',
+        headers : {
+            sessiontoken : localStorage.getItem( 'token' )
+        }
+    };
+
+    fetch( url, settings )
+        .then( response => {
+            if( response.ok ) {
+                return response.json();
+            }
+            throw new Error( response.statusText ); 
+        })
+        .then( responseJSON => {
+            userEmail( responseJSON );
+        })
+        .catch( err => {
+            console.log( err.message );
+            window.location.href = "/";
+        });
+}
+
+function userEmail( data ) {
+    console.log( data );
+    if( data.superuser == false ) {
+        window.location.href = "/inicio/";
+    } 
+}
+
+function logout() {
+    localStorage.setItem( 'token', null );
+    init();
+    console.log("you are not logged in.");
+}
+
+function init() {
+    validate();
+   let str = window.location.href;
+   let substr = str.substr(21, str.length);
+   console.log( "Estamos en la pagina de: " + substr );
+}
+
+init();
