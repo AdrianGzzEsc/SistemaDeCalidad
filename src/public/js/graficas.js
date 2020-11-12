2dd8d77f47c77247b64f39879494e80f4cba5872
OmarLa98 <omarlopez.789@gmail.com>
2020-11-12 15:23:47 -0600

    Filtro de semanas para las graficas front y back

--- src/public/js/graficas.js
+++ src/public/js/graficas.js
@@ -2,101 +2,27 @@ function init() {
 
 
 
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    dateWeek = 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7)
+    dateYear = date.getFullYear()
+    defaultDate = dateYear + "-W" + dateWeek
+    document.getElementById("filtro-semana").value = defaultDate
+    var d = ((dateWeek - 1) * 7) - 1;
 
-    names = ['Defectos en Proceso - Escuadradora', 'Defectos en Proceso - Enchapadora', 'Defectos en Proceso - Taladro', 'Defectos en Proceso - Sacabocas', 'Defectos en Proceso - Armado1', 'Defectos en Proceso - Armado2', 'Defectos en Proceso - Armado3', 'Defectos en Proceso - Acabados'];
-    fechas = ["Fecha-1", "Fecha-2", "Fecha-3", "Fecha-4", "Fecha-5", "Total"]
+    // primer dia de la semana en formato fecha
+    dateFDW = new Date(dateYear, 0, d)
 
-    for (var iI = 0; iI < 8; iI++) {
+    console.log(dateFDW)
 
-        const sec = document.createElement('section');
-        const container = document.createElement('div');
-        const row = document.createElement('div');
-        const col = document.createElement('div');
-        const card = document.createElement('div');
-        const cardTitle = document.createElement('h3');
-        container.classList.add("container");
-        row.classList.add('row');
-        col.classList.add("col", "mb-2", "mt-4", "border-light");
-        card.classList.add("card", "rounded-0", "border-0", "mt-2");
-        cardTitle.classList.add("card-title", "pl-1");
-        cardTitle.style.backgroundColor = "gray";
-        cardTitle.style.color = "white";
-        cardTitle.appendChild(document.createTextNode(names[iI]));
-        card.appendChild(cardTitle);
-        col.appendChild(card);
-        row.appendChild(col);
-        container.appendChild(row);
-        sec.appendChild(container);
-        document.body.appendChild(sec);
+    semana = document.getElementById("filtro-semana")
+    semana.addEventListener("change", event => {
+        event.preventDefault()
+        console.log(event.target.value)
 
-        const thVacio = document.createElement('th');
-        const thCont = document.createElement('th');
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
-        const trTotal = document.createElement('tr');
-        const trApr = document.createElement('tr');
-        const trRch = document.createElement('tr');
-        const table = document.createElement('table');
-        const section2 = document.createElement('section');
-        const container2 = document.createElement('div');
-        const row2 = document.createElement('div');
-        const col2 = document.createElement('div');
-        const tableResp = document.createElement('div');
-        //thVacio.scope('col')
-        thVacio.style.borderRadius = "0px";
-        thCont.style.borderRadius = "0px";
-        //thCont.scope("col");
-        table.classList.add('table');
-        container2.classList.add("container");
-        row2.classList.add('row');
-        col2.classList.add("col-lg-7", "my-3", "pb-0");
-        tableResp.classList.add("table-responsive-md");
-
-        thCont.appendChild(thVacio);
-
-        for (var iJ = 0; iJ < 6; iJ++) {
-            const thCol = document.createElement('th');
-            //thCol.scope.add("col");
-            thCol.style.backgroundColor = "orangered";
-            thCol.style.borderRadius = "1px";
-            thCol.style.borderColor = "black";
-            thCol.appendChild(document.createTextNode(fechas[iJ]));
-            thCont.appendChild(thCol);
-        }
-        thead.appendChild(thCont);
-        table.appendChild(thead);
-        const thTotal = document.createElement('th');
-        //thTotal.scope('row');
-        thTotal.style.borderRadius = "0px";
-        const bold = document.createElement('b');
-        bold.appendChild(document.createTextNode("Total"))
-        thTotal.appendChild(bold);
-        const tdVacio = document.createElement('td');
-        tdVacio.style.border = "0px";
-        trTotal.appendChild(tdVacio);
-        /*
-        for (var iJ = 0; iJ < 6; iJ++) {
-            const thCol = document.createElement('th');
-            //thCol.scope.add("col");
-            thCol.style.backgroundColor = "orangered";
-            thCol.style.borderRadius = "1px";
-            thCol.style.borderColor = "black";
-            thCol.appendChild(document.createTextNode(fechas[iJ]));
-            thCont.appendChild(thCol);
-        }*/
-        tbody.appendChild(trTotal);
-        table.appendChild(tbody);
-
-        tableResp.appendChild(table);
-        col2.appendChild(tableResp);
-        row2.appendChild(col2);
-        container2.appendChild(row2);
-        section2.appendChild(container2);
-        document.body.appendChild(section2);
-
-    }
-
-
+    })
 
     var data = [1, 2, 3, 4, 5, 6, 7];
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -138,5 +64,6 @@ function init() {
         options: {}
     });
 };
+
 
 init();
